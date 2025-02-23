1f01395e3b70b814f57628a2f944c6b68c5f695e
LakshyaGitHub98 <laktripathi9839@gmail.com>
2025-02-23 11:35:19 +0530

    Adding Image in application

--- practise_app/lib/files/Image.dart
+++ practise_app/lib/files/Image.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Demo Home Page'),
+      home: const MyHomePage(title: 'Image Example'),
     );
   }
 }
@@ -30,29 +30,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body:Column(
-        children: [
-          Center(
-            child: Container(
-              color: Colors.blue,
-              child:Text("Welcome to My First App",style: TextStyle(color:Colors.white,height: 20),),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Center(child: Text(widget.title)),
+        ),
+        body:Column(
+          children: [
+            Center(
+              child: Container(
+                width: 500,
+                height: 700,
+                child: Image.asset("assets/images/Photo1.jpg"),
+              ),
             ),
-          ),
-          Center(
-            child: Container(
-              child: Text("Show your love ❤️"),
-            ),
-          )
-        ],
-      ),
+            Text("Just an start 🫠",style:TextStyle(color:Colors.red,fontWeight:FontWeight.w700),)
+          ],
+        )
     );
   }
 }
