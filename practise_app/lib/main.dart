89917e7c31c96dd314e70c333bcd71d132263483
LakshyaGitHub98 <laktripathi9839@gmail.com>
2025-06-02 17:12:17 +0530

    Adding gradient feature in main.dart

--- practise_app/lib/main.dart
+++ practise_app/lib/main.dart
@@ -46,49 +46,58 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center( // Ensures everything is centralized
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(12.0),
-                boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 8)],
-              ),
-              child: const Text(
-                "Welcome to My First App",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24, // Larger font size for emphasis
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: _increaseCounter,
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.all(12.0),
-                foregroundColor: Colors.deepPurple,
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              child: const Text("Show your love ❤️"),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "❤️ Count: $_counter",
-              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors:[Colors.black, Colors.deepPurple, Colors.blueGrey],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )
         ),
+        child: Center( // Ensures everything is centralized
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  // padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(12.0),
+                    boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 18)],
+                  ),
+                  child: const Text(
+                    "Welcome to My App",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24, // Larger font size for emphasis
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: _increaseCounter,
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(12.0),
+                    foregroundColor: Colors.deepPurple,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  child: const Text("Show your love ❤️",style: TextStyle(color:Colors.black),),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "❤️ Count: $_counter",
+                  style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _increaseCounter,
         backgroundColor: Colors.deepPurple,
         child: const Icon(Icons.favorite, color: Colors.white),
-      ),
+    )
     );
   }
 }
